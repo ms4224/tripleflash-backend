e605f235dc04434c0f1d27a9e9ebf65dbca4dcd3
Matias <schakelmatias@gmail.com>
2020-11-22 17:39:51 +0900

    Fixed callback stuff and res.send syntax error

--- server/routes/CardsRoutes.js
+++ server/routes/CardsRoutes.js
@@ -22,9 +22,12 @@ router.post('/decks', function(req, res){
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         console.log('updating decks route!');
         console.log('req----------------------------------------------',req.body);
-        cardFncs.newDeck(req.body.deck, () => {
+        cardFncs.newDeck(req.body.deck, (err, queryResult) => {
             console.log('done updating.')
             res.status(200);
+            if (err) {
+                console.log('there was an error!', err)
+            }
             res.send({message: 'done updating'});
         })
 })
@@ -34,9 +37,12 @@ router.post('/cards', function(req, res){
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         console.log('updating flashcard route!');
         console.log('req----------------------------------------------',req.body);
-        cardFncs.insertFlashCard(req.body.kanji, req.body.hiragana, req.body.english, req.body.deck, req.body.sampleSentence, req.body.tag, () => {
+        cardFncs.insertFlashCard(req.body.kanji, req.body.hiragana, req.body.english, req.body.deck, req.body.sampleSentence, req.body.tag, (err, queryResult) => {
             console.log('done updating.')
             res.status(200);
+            if (err) {
+                console.log('there was an error!', err)
+            }
             res.send({message: 'done updating'});
         })
 })
@@ -46,10 +52,13 @@ router.get('/cards/:deck', function(req, res) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     console.log('getting flashcards!');
     console.log('requested deck',req.params.deck);
-    cardFncs.getCards(req.params.deck, () => {
+    cardFncs.getCards(req.params.deck, (err, queryResult) => {
         console.log('done retrieving cards.')
         res.status(200);
-        res.send(res);
+        if (err) {
+            console.log('there was an error!', err)
+        }
+        res.send(queryResult);
     })
 })
 router.get('/decks', function(req, res) {
@@ -57,10 +66,13 @@ router.get('/decks', function(req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     console.log('getting decks!');
-    cardFncs.getDecks(() => {
+    cardFncs.getDecks((err, queryResult) => {
         console.log('done retrieving decks.')
         res.status(200);
-        res.send(res);
+        if (err) {
+            console.log('there was an error!', err)
+        }
+        res.send(queryResult);
     })
 })
 
--- server/src/cards.js
+++ server/src/cards.js
@@ -5,7 +5,7 @@ function insertFlashCard(kanji, hiragana, english, deck, sampleSentence, tag, cb
     var queryString = `INSERT INTO flashcard_test values (${kanji}, ${hiragana}, ${english}, ${deck}, ${sampleSentence}, ${tag})`;
     pgConnect.executeQuery(queryString, () => {
         console.log('finished querying.  queryString was ' + queryString);
-        if (cb) cb();
+        if (cb) cb(err, queryRes);
     })
 }
 
@@ -13,7 +13,7 @@ function newDeck(deckName, cb) {
     var queryString = `INSERT INTO decks values (${deckName})`;
     pgConnect.executeQuery(queryString, () => {
         console.log('finished querying.  queryString was ' + queryString);
-        if (cb) cb();
+        if (cb) cb(err, queryRes);
     })
 }
 
@@ -21,7 +21,7 @@ function getDecks(cb) {
     var queryString = 'SELECT * FROM decks';
     pgConnect.executeQuery(queryString, () => {
         console.log('finished querying.  queryString was ' + queryString);
-        if (cb) cb();
+        if (cb) cb(err, queryRes);
     })
 }
 
@@ -29,7 +29,7 @@ function getCards(deckName, cb) {
     var queryString  = `SELECT * FROM flashcard_test WHERE deck = '${deckName}'`;
     pgConnect.executeQuery(queryString, () => {
         console.log('finished querying.  queryString was ' + queryString);
-        if (cb) cb();
+        if (cb) cb(err, queryRes);
     })
 }
 
