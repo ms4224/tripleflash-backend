2d3454199075382068fa45567618bb04e49a1308
Matias <schakelmatias@gmail.com>
2020-11-22 17:42:10 +0900

    Fixed another callback error

--- server/src/cards.js
+++ server/src/cards.js
@@ -3,7 +3,7 @@ var pgConnect = require('./postgrePoolConnector');
 
 function insertFlashCard(kanji, hiragana, english, deck, sampleSentence, tag, cb) {
     var queryString = `INSERT INTO flashcard_test values (${kanji}, ${hiragana}, ${english}, ${deck}, ${sampleSentence}, ${tag})`;
-    pgConnect.executeQuery(queryString, () => {
+    pgConnect.executeQuery(queryString, (err, queryRes) => {
         console.log('finished querying.  queryString was ' + queryString);
         if (cb) cb(err, queryRes);
     })
@@ -11,7 +11,7 @@ function insertFlashCard(kanji, hiragana, english, deck, sampleSentence, tag, cb
 
 function newDeck(deckName, cb) {
     var queryString = `INSERT INTO decks values (${deckName})`;
-    pgConnect.executeQuery(queryString, () => {
+    pgConnect.executeQuery(queryString, (err, queryRes) => {
         console.log('finished querying.  queryString was ' + queryString);
         if (cb) cb(err, queryRes);
     })
@@ -19,7 +19,7 @@ function newDeck(deckName, cb) {
 
 function getDecks(cb) {
     var queryString = 'SELECT * FROM decks';
-    pgConnect.executeQuery(queryString, () => {
+    pgConnect.executeQuery(queryString, (err, queryRes) => {
         console.log('finished querying.  queryString was ' + queryString);
         if (cb) cb(err, queryRes);
     })
@@ -27,7 +27,7 @@ function getDecks(cb) {
 
 function getCards(deckName, cb) {
     var queryString  = `SELECT * FROM flashcard_test WHERE deck = '${deckName}'`;
-    pgConnect.executeQuery(queryString, () => {
+    pgConnect.executeQuery(queryString, (err, queryRes) => {
         console.log('finished querying.  queryString was ' + queryString);
         if (cb) cb(err, queryRes);
     })
